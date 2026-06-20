4d4fcb1e2c2cd65a8ecfbca4fc3ed42d94f3a092
lebogang <lebomodika619@gmail.com>
2026-06-20 06:54:52 +0200

    Add setuptools to restore distutils compatibility for Django 2.2

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -20,7 +20,8 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 python3 -m venv $PROJECT_BASE_PATH/env
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi
+# Added uwsgi and setuptools for full Python 3.12 compatibility
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi setuptools
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
